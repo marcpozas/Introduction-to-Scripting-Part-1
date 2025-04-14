74dbbd5733fbbc6444ef182a0635a77a7549cbc4
Marc <marc.pozas01@estudiant.upf.edu>
2025-04-14 23:46:41 +0200

    Initial commit

---
+++ Assets/RW/Scripts/Managers/GameStateManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour
+{
+    public static GameStateManager Instance; // 1
+
+    [HideInInspector]
+    public int sheepSaved; // 2
+
+    [HideInInspector]
+    public int sheepDropped; // 3
+
+    public int sheepDroppedBeforeGameOver; // 4
+    public SheepSpawner sheepSpawner; // 5
+
+    public GameObject iceOverlayUI;
+
+    private Coroutine slowTimeCoroutine;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+
+    public void SavedSheep()
+    {
+        sheepSaved++;
+        UIManager.Instance.UpdateSheepSaved();
+    }
+
+    private void GameOver()
+    {
+        sheepSpawner.canSpawn = false; // 1
+        sheepSpawner.DestroyAllSheep(); // 2
+        UIManager.Instance.ShowGameOverWindow();
+    }
+
+    public void DroppedSheep()
+    {
+        sheepDropped++; // 1
+        UIManager.Instance.UpdateSheepDropped();
+        Debug.Log(sheepDropped);
+
+        if (sheepDropped == sheepDroppedBeforeGameOver) // 2
+        {
+            GameOver();
+        }
+    }
+
+    public void ActivateSlowDownEffect()
+    {
+        if (slowTimeCoroutine == null)
+        {
+            slowTimeCoroutine = StartCoroutine(SlowDownTime());
+        }
+    }
+
+    private IEnumerator SlowDownTime()
+    {
+        Time.timeScale = 0.5f;
+
+        if (iceOverlayUI != null)
+            iceOverlayUI.SetActive(true);
+
+        yield return new WaitForSecondsRealtime(3f);
+
+        Time.timeScale = 1f;
+
+        if (iceOverlayUI != null)
+            iceOverlayUI.SetActive(false);
+
+        slowTimeCoroutine = null;
+    }
+}
---
+++ Assets/RW/Scripts/SheepSpawner.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepSpawner : MonoBehaviour
+{
+    public bool canSpawn = true; // 1
+    public GameObject sheepPrefab; // 2
+    public GameObject blueSheepPrefab;
+    public List<Transform> sheepSpawnPositions = new List<Transform>(); // 3
+    public float timeBetweenSpawns;
+
+    private List<GameObject> sheepList = new List<GameObject>(); // 5
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator SpawnRoutine() // 1
+    {
+        while (canSpawn) // 2
+        {
+            SpawnSheep(); // 3
+            yield return new WaitForSeconds(timeBetweenSpawns); // 4
+        }
+    }
+
+    public void RemoveSheepFromList(GameObject sheep)
+    {
+        sheepList.Remove(sheep);
+    }
+
+    private void SpawnSheep()
+    {
+        GameObject prefabToUse = sheepPrefab;
+        // 10% probability that the ice sheep appears
+        if (Random.value < 0.5f)
+        {
+            prefabToUse = blueSheepPrefab;
+        }
+        Vector3 randomPosition = sheepSpawnPositions[Random.Range(0, sheepSpawnPositions.Count)].position; // 1
+        GameObject sheep = Instantiate(prefabToUse, randomPosition, prefabToUse.transform.rotation); // 2
+        sheepList.Add(sheep); // 3
+        sheep.GetComponent<Sheep>().SetSpawner(this); // 4
+        Debug.Log("Spawned sheep: " + prefabToUse.name);
+    }
+
+    public void DestroyAllSheep()
+    {
+        foreach (GameObject sheep in sheepList) // 1
+        {
+            Destroy(sheep); // 2
+        }
+
+        sheepList.Clear();
+    }
+}
